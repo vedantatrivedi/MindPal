429e59fffee96ffad11538da6768ecd4a5343e6c
Vedanta Trivedi <vedantatrivedi.trivedi@gmail.com>
2021-10-22 17:15:01 +0530

    adding basic changes

--- model/__init__.py
+++ model/__init__.py
@@ -20,8 +20,9 @@ def checkloginusername():
     else:
         return "User exists"
 
+
 def checkloginpassword():
-    
+
     username = request.form["username"]
     check = db.users.find_one({"username": username})
 
@@ -47,10 +48,11 @@ def checkusername():
     else:
         return "Username taken"
 
+
 def registerUser():
-    fields = [k for k in request.form]                                      
+    fields = [k for k in request.form]
     values = [request.form[k] for k in request.form]
-    
+
     fields.append("posts")
     values.append([])
 
@@ -59,10 +61,10 @@ def registerUser():
 
     data = dict(zip(fields, values))
 
-    username    = data['username']
+    username = data['username']
     exist_count = db.users.find({"username": username}).count()
 
-    if(exist_count>0):
+    if(exist_count > 0):
         return False
     else:
         user_data = json.loads(json_util.dumps(data))
@@ -70,6 +72,7 @@ def registerUser():
         user_data["confirmpassword"] = getHashed(user_data["confirmpassword"])
         db.users.insert(user_data)
         return True
+
 
 def addTrustedUser(username):
     fields = [k for k in request.form]
@@ -80,33 +83,36 @@ def addTrustedUser(username):
     values.append(username)
     db.trusted.insert(user_data)
     trustedUser = request.form.get("username")
-    db.users.update({"username": username}, {"$push": {"trustedUser": trustedUser}})
+    db.users.update({"username": username}, {
+                    "$push": {"trustedUser": trustedUser}})
     print("Done")
 
-    
+
 # Post structure - [Date, Post, Score]
 def addPost(user, post):
     score = get_sentiment(post)
     today = datetime.today()
-    db.users.update( {"username": user}, {"$push": {"posts": [today, post, score]}} )
+    db.users.update({"username": user}, {
+                    "$push": {"posts": [today, post, score]}})
     print("Post Added")
 
 
 def getPost(username):
     print("Posts retrieved")
-    myquery = { "username": username }
+    myquery = {"username": username}
     userDoc = db.users.find_one(myquery)
 
     if(userDoc == None):
         return []
 
-    items   = userDoc["posts"]
+    items = userDoc["posts"]
     # print(items[-1])
     return items[::-1]
 
+
 def getScores(username):
 
-    myquery = { "username": username }
+    myquery = {"username": username}
     userDoc = db.users.find_one(myquery)
     posts = userDoc["posts"]
     scores = []
@@ -138,33 +144,38 @@ def getScores(username):
 
 #     for entry in post:
 #         days.append(calendar.day_name[entry[0].weekday()])
-       
+
 #     return days
+
+
 def getStreak(username):
     # All the number of consecutive days where entry exists
-    
-    return 
-    
+
+    return
+
+
 def currentStreak(username):
     # Number of entries from today's date to backwards
-    return  
-    
-    
+    return
+
+
 def getFormattedDate(date):
     return date.strftime('%b %d, %Y')
+
 
 def getDayfromDate(date):
     return calendar.day_name[date.weekday()]
 
-    
+
 def getScoresForChart(username):
     scores = getScores(username)[-7:]
     return_score = np.array(scores).flatten().tolist()
     rounded_scores = [round(num) for num in return_score]
     return rounded_scores
 
+
 def getScoresForPieChart(username):
-    user = db.users.find_one({"username":session["username"]})
+    user = db.users.find_one({"username": session["username"]})
     print(session["username"])
     # print(user)
     scores = user["scores"]
@@ -172,13 +183,15 @@ def getScoresForPieChart(username):
     rounded_scores = [round(num) for num in return_score]
     print(rounded_scores)
     happy_elements = [element for element in rounded_scores if element >= 90]
-    med_elements = [element for element in rounded_scores if element >= 70 and element < 90]
+    med_elements = [
+        element for element in rounded_scores if element >= 70 and element < 90]
     unhappy_elements = [element for element in rounded_scores if element < 70]
     happy_score = len(happy_elements)
     med_score = len(med_elements)
     unhappy_score = len(unhappy_elements)
-    pie_chart_data = [happy_score,med_score,unhappy_score]
+    pie_chart_data = [happy_score, med_score, unhappy_score]
     return pie_chart_data
+
 
 def checkOAuthToken():
 
