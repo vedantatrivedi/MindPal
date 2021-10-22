2e45bff71b7abd6383828b2c06a2d68a49dd6356
Jinit Shah <jinit245@gmail.com>
2021-10-22 16:48:17 +0530

    Req changed, Trusted User Temp add

--- model/email_service.py
+++ model/email_service.py
@@ -9,7 +9,7 @@ from email.mime.text import MIMEText
 import lxml.html
 
 from jinja2 import Environment, FileSystemLoader
-import os 
+import os
 
 GOOGLE_ACCOUNTS_BASE_URL = 'https://accounts.google.com'
 REDIRECT_URI = 'http://127.0.0.1:5000/'
@@ -17,7 +17,9 @@ GOOGLE_CLIENT_ID = '526537437699-t8hafs917rs1np2kgu6ibh84vra68neq.apps.googleuse
 GOOGLE_CLIENT_SECRET = 'EcWvRZxscnVeXTEnWivMTuZW'
 
 
-env = Environment(loader = FileSystemLoader('%s/static/email_template' % os.getcwd()))
+env = Environment(loader=FileSystemLoader(
+    '%s/static/email_template' % os.getcwd()))
+
 
 def command_to_url(command):
     return '%s/%s' % (GOOGLE_ACCOUNTS_BASE_URL, command)
@@ -55,7 +57,8 @@ def call_authorize_tokens(client_id, client_secret, authorization_code):
     params['redirect_uri'] = REDIRECT_URI
     params['grant_type'] = 'authorization_code'
     request_url = command_to_url('o/oauth2/token')
-    response = urllib.request.urlopen(request_url, urllib.parse.urlencode(params).encode('UTF-8')).read().decode('UTF-8')
+    response = urllib.request.urlopen(request_url, urllib.parse.urlencode(
+        params).encode('UTF-8')).read().decode('UTF-8')
     return json.loads(response)
 
 
@@ -66,49 +69,41 @@ def call_refresh_token(client_id, client_secret, refresh_token):
     params['refresh_token'] = refresh_token
     params['grant_type'] = 'refresh_token'
     request_url = command_to_url('o/oauth2/token')
-    response = urllib.request.urlopen(request_url, urllib.parse.urlencode(params).encode('UTF-8')).read().decode('UTF-8')
+    response = urllib.request.urlopen(request_url, urllib.parse.urlencode(
+        params).encode('UTF-8')).read().decode('UTF-8')
     return json.loads(response)
 
 
 def generate_oauth2_string(username, access_token, as_base64=False):
     auth_string = 'user=%s\1auth=Bearer %s\1\1' % (username, access_token)
     if as_base64:
-        auth_string = base64.b64encode(auth_string.encode('ascii')).decode('ascii')
+        auth_string = base64.b64encode(
+            auth_string.encode('ascii')).decode('ascii')
     return auth_string
-
-
-def test_imap(user, auth_string):
-    imap_conn = imaplib.IMAP4_SSL('imap.gmail.com')
-    imap_conn.debug = 4
-    imap_conn.authenticate('XOAUTH2', lambda x: auth_string)
-    imap_conn.select('INBOX')
-
-
-def test_smpt(user, base64_auth_string):
-    smtp_conn = smtplib.SMTP('smtp.gmail.com', 587)
-    smtp_conn.set_debuglevel(True)
-    smtp_conn.ehlo('test')
-    smtp_conn.starttls()
-    smtp_conn.docmd('AUTH', 'XOAUTH2 ' + base64_auth_string)
 
 
 def get_authorization(google_client_id, google_client_secret):
     scope = "https://mail.google.com/"
-    print('Navigate to the following URL to auth:', generate_permission_url(google_client_id, scope))
+    print('Navigate to the following URL to auth:',
+          generate_permission_url(google_client_id, scope))
     authorization_code = input('Enter verification code: ')
-    response = call_authorize_tokens(google_client_id, google_client_secret, authorization_code)
+    response = call_authorize_tokens(
+        google_client_id, google_client_secret, authorization_code)
     return response['refresh_token'], response['access_token'], response['expires_in']
 
 
 def refresh_authorization(google_client_id, google_client_secret, refresh_token):
-    response = call_refresh_token(google_client_id, google_client_secret, refresh_token)
+    response = call_refresh_token(
+        google_client_id, google_client_secret, refresh_token)
     return response['access_token'], response['expires_in']
 
 
 def send_mail(fromaddr, toaddr, subject, message, refresh_token):
 
-    access_token, expires_in = refresh_authorization(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, refresh_token)
-    auth_string = generate_oauth2_string(fromaddr, access_token, as_base64=True)
+    access_token, expires_in = refresh_authorization(
+        GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, refresh_token)
+    auth_string = generate_oauth2_string(
+        fromaddr, access_token, as_base64=True)
 
     msg = MIMEMultipart('related')
     msg['Subject'] = subject
@@ -117,7 +112,8 @@ def send_mail(fromaddr, toaddr, subject, message, refresh_token):
     msg.preamble = 'This is a multi-part message in MIME format.'
     msg_alternative = MIMEMultipart('alternative')
     msg.attach(msg_alternative)
-    part_text = MIMEText(lxml.html.fromstring(message).text_content().encode('utf-16'), 'plain', _charset='utf-16')
+    part_text = MIMEText(lxml.html.fromstring(
+        message).text_content().encode('utf-16'), 'plain', _charset='utf-16')
     part_html = MIMEText(message.encode('utf-16'), 'html', _charset='utf-16')
     msg_alternative.attach(part_text)
     msg_alternative.attach(part_html)
@@ -132,13 +128,10 @@ def send_mail(fromaddr, toaddr, subject, message, refresh_token):
 def send_welcome_mail(fromaddr, toaddr, user_name, refresh_token):
 
     template = env.get_template('welcome_email.html')
-    output = template.render(username = user_name)
-    send_mail(fromaddr, toaddr, "Welcome to Muse!", output, refresh_token)    
+    output = template.render(username=user_name)
+    send_mail(fromaddr, toaddr, "Welcome to Muse!", output, refresh_token)
 
-    return "Mail sent successfully."
-
-
-
+    return True
 
 
 # if __name__ == '__main__':
--- views/__init__.py
+++ views/__init__.py
@@ -2,21 +2,14 @@ from flask import render_template, request, redirect, url_for, session, flash
 from app import app
 from model import *
 from model import email_service
-from functools import reduce
-import numpy as np
-from operator import add
 import json
-
-import os
-import requests
-
-import google.oauth2.credentials
 import google_auth_oauthlib.flow
-import googleapiclient.discovery
 
 
 CLIENT_SECRETS_FILE = "client_secret.json"
-SCOPES = ["https://mail.google.com/", "https://www.googleapis.com/auth/gmail.send", "https://www.googleapis.com/auth/drive.metadata.readonly"]
+SCOPES = ["https://mail.google.com/", "https://www.googleapis.com/auth/gmail.send",
+          "https://www.googleapis.com/auth/drive.metadata.readonly"]
+
 
 @app.route('/', methods=["GET"])
 def home():
@@ -34,69 +27,77 @@ def home():
             scores.append(entry[2])
             days.append(getDayfromDate(entry[0]))
 
-        return render_template('index.html',username = session["username"], posts = text,scores = scores, days = days, dates = dates)
+        return render_template('index.html', username=session["username"], posts=text, scores=scores, days=days, dates=dates)
     else:
         return render_template('login.html')
 
 # Register new user
 @app.route('/register', methods=["GET", "POST"])
 def register():
+    
     if request.method == "GET":
         return render_template("register.html")
+
     elif request.method == "POST":
+
         response = registerUser()
+
         # Send Welcome Mail
         if(checkOAuthToken() and response):
+            
             token = getOAuthToken()
-            email_service.send_welcome_mail('dornumofficial@gmail.com', request.form['email'], request.form['name'], token['refresh_token'])
+            email_service.send_welcome_mail(
+                'dornumofficial@gmail.com', request.form['email'], request.form['name'], token['refresh_token'])
+
             return redirect(url_for("login"))
-        else: 
+        else:
+
             flash("Username already exists")
             return render_template('register.html')
-    
 
-#Check if email already exists in the registratiion page
+
+# Check if email already exists in the registratiion page
 @app.route('/checkusername', methods=["POST"])
 def check():
     return checkusername()
 
-@app.route('/addPost',methods = ["POST"])
+
+@app.route('/addPost', methods=["POST"])
 def addPosts():
 
     response = {
-      'status': 400,
+        'status': 400,
     }
 
     try:
         addPost(session['username'], request.form['ckeditor'])
         response = {
-          'status': 201,
+            'status': 201,
         }
 
     except:
         pass
 
     return redirect('/')
-    
-@app.route('/getPosts', methods = ["Get"])
+
+
+@app.route('/getPosts', methods=["Get"])
 def getPosts():
-    
+
     response = {
-      'status': 400,
-      'body'  : '',
+        'status': 400,
+        'body': '',
     }
 
     try:
         response = {
-          'status': 200,
-          'body'  : json.dumps(getPost(session["username"])),
+            'status': 200,
+            'body': json.dumps(getPost(session["username"])),
         }
     except:
         pass
-    
 
     return response
-
 
 
 # Everything Login (Routes to renderpage, check if username exist and also verifypassword through Jquery AJAX request)
@@ -108,103 +109,104 @@ def login():
         else:
             return redirect(url_for("home"))
 
-@app.route('/userlogin', methods=["GET","POST"])
+
+@app.route('/userlogin', methods=["GET", "POST"])
 def userlogin():
     if request.method == "GET":
         return render_template("userlogin.html")
     else:
         scores = getScoresForChart("test")
-        labels= ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-        return render_template("trustedUserDashboard.html",scores = scores,labels = labels)
-
+        labels = ["Monday", "Tuesday", "Wednesday",
+                  "Thursday", "Friday", "Saturday", "Sunday"]
+        return render_template("trustedUserDashboard.html", scores=scores, labels=labels)
 
 
 @app.route('/checkloginusername', methods=["POST"])
 def checkUserlogin():
     return checkloginusername()
 
+
 @app.route('/checkloginpassword', methods=["POST"])
 def checkUserpassword():
     return checkloginpassword()
 
-#The admin logout
+# The admin logout
 @app.route('/logout', methods=["GET"])  # URL for logout
 def logout():  # logout function
     session.pop('username', None)  # remove user session
     return redirect(url_for("home"))  # redirect to home page with message
 
-#Forgot Password
+# Forgot Password
 @app.route('/forgot-password', methods=["GET"])
 def forgotpassword():
     return render_template('forgot-password.html')
 
-#404 Page
+# 404 Page
 @app.route('/404', methods=["GET"])
 def errorpage():
     return render_template("404.html")
 
-#Blank Page
+# Blank Page
 @app.route('/blank', methods=["GET"])
 def blank():
     return render_template('blank.html')
 
-#Buttons Page
+# Buttons Page
 @app.route('/buttons', methods=["GET"])
 def buttons():
     return render_template("buttons.html")
 
-#Cards Page
+# Cards Page
 @app.route('/cards', methods=["GET"])
 def cards():
     return render_template('cards.html')
 
-#Charts Page
+# Charts Page
 @app.route('/charts', methods=["GET"])
 def charts():
     scores = getScoresForChart(session["username"])
-    labels= ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-    return render_template("charts.html",scores = scores,labels = labels,user=session["username"])
+    labels = ["Monday", "Tuesday", "Wednesday",
+              "Thursday", "Friday", "Saturday", "Sunday"]
+    return render_template("charts.html", scores=scores, labels=labels, user=session["username"])
 
-#Tables Page
+# Tables Page
 @app.route('/tables', methods=["GET"])
 def tables():
     return render_template("tables.html")
 
-#Utilities-animation
+# Utilities-animation
 @app.route('/utilities-animation', methods=["GET"])
 def utilitiesanimation():
     return render_template("utilities-animation.html")
 
-#Utilities-border
-@app.route('/utilities-border', methods=["GET","POST"])
+# Utilities-border
+@app.route('/utilities-border', methods=["GET", "POST"])
 def utilitiesborder():
     return render_template("utilities-border.html")
 
-#Utilities-color
-@app.route('/add-trusted-user', methods=["GET","POST"])
+# Utilities-color
+@app.route('/add-trusted-user', methods=["GET", "POST"])
 def addTrustedUser():
-    
+
     if request.method == "GET":
         return render_template("add_trusted_user.html")
     elif request.method == "POST":
         addTrustedUser(session["username"])
-        return render_template('index.html',username = session["username"], 
-             posts = getPost(session["username"]),scores = getScores(session["username"]), dates=getDates(session["username"]))
+        return render_template('index.html', username=session["username"],
+                               posts=getPost(session["username"]), scores=getScores(session["username"]), dates=getDates(session["username"]))
 
-#utilities-other
+# utilities-other
 @app.route('/utilities-other', methods=["GET"])
 def utilitiesother():
     return render_template("utilities-other.html")
 
-
+# Testing endpoint for sending mail 
 # @app.route('/send_mail', methods=["GET"])
-
 # def send_mail():
-#     email_service.send_mail('dornumofficial@gmail.com', 'dornumofficial@gmail.com',
-#               'A mail from you from Python',
-#               '<b>A mail from you from Python</b><br>', session['credentials']['refresh_token'])
-
-#     return ""
+#     token = getOAuthToken()
+#     email_service.send_welcome_mail(
+#         'dornumofficial@gmail.com', 'dornumofficial@gmail.com', "SA", token['refresh_token'])
+#     return redirect(url_for("home"))
 
 
 @app.route('/authorize', methods=["GET"])
@@ -212,7 +214,7 @@ def authorize():
 
     # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-      CLIENT_SECRETS_FILE, scopes=SCOPES)
+        CLIENT_SECRETS_FILE, scopes=SCOPES)
 
     # The URI created here must exactly match one of the authorized redirect URIs
     # for the OAuth 2.0 client, which you configured in the API Console. If this
@@ -221,33 +223,33 @@ def authorize():
     flow.redirect_uri = url_for('oauth2callback', _external=True)
 
     authorization_url, state = flow.authorization_url(
-      # Enable offline access so that you can refresh an access token without
-      # re-prompting the user for permission. Recommended for web server apps.
-      access_type='offline',
-      # Enable incremental authorization. Recommended as a best practice.
-      include_granted_scopes='true')
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+        # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes='true')
 
     if(checkOAuthToken()):
         return redirect('/')
     else:
         session['state'] = state
-    
+
     return redirect(authorization_url)
 
-@app.route('/oauth2callback', methods=["GET","POST"])
+
+@app.route('/oauth2callback', methods=["GET", "POST"])
 def oauth2callback():
 
     # Specify the state when creating the flow in the callback so that it can
     # verified in the authorization server response.
     state = session['state']
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-      CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
     flow.redirect_uri = url_for('oauth2callback', _external=True)
 
     # Use the authorization server's response to fetch the OAuth 2.0 tokens.
     authorization_response = request.url
-    flow.fetch_token(authorization_response = authorization_response)
-
+    flow.fetch_token(authorization_response=authorization_response)
 
     # Store credentials in the session.
     # ACTION ITEM: In a production app, you likely want to save these
@@ -258,10 +260,36 @@ def oauth2callback():
 
     return redirect('/')
 
+@app.route('/set_password', methods=["GET", "POST"])
+def set_password():
+
+    if request.method == "GET":
+        return render_template("trusted_user_set_password.html")
+
+    elif request.method == "POST":
+
+        response = registerUser()
+
+        # Send Welcome Mail
+        if(checkOAuthToken() and response):
+            
+            token = getOAuthToken()
+            email_service.send_welcome_mail(
+                'dornumofficial@gmail.com', request.form['email'], request.form['name'], token['refresh_token'])
+
+            return redirect(url_for("login"))
+        else:
+
+            flash("Username already exists")
+            return render_template('register.html')
+
+
+
+
 def credentials_to_dict(credentials):
-  return {'token': credentials.token,
-          'refresh_token': credentials.refresh_token,
-          'token_uri': credentials.token_uri,
-          'client_id': credentials.client_id,
-          'client_secret': credentials.client_secret,
-          'scopes': credentials.scopes}
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes}
