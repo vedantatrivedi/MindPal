44a282c8da3c4da90d143d07b78aac5643fc4329
Jinit Shah <jinit245@gmail.com>
2021-11-01 19:09:28 +0530

    UI

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_ckeditor import CKEditor
 import ssl
+# from waitress import serve
 
 app = Flask(__name__)
 ckeditor = CKEditor(app)
@@ -11,4 +12,5 @@ from views import *
 if __name__ == "__main__":
     context = ssl.SSLContext()
     context.load_cert_chain('server.crt', 'server.key')
+    # serve(app, host="127.0.0.1", port=500)
     app.run(ssl_context = context, debug="true")
