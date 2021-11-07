d6db0313ec692055bc998a028997f4faa433c8c0
Vedanta Trivedi <vedantatrivedi.trivedi@gmail.com>
2021-11-07 16:03:47 +0530

    Cleanup and gitignore

--- model/oauth.py
+++ model/oauth.py
@@ -31,4 +31,12 @@ def putOAuthToken(credentials):
 def getOAuthToken():
     return oauthdb.OAuth_tokens.find_one()
 
+def credentials_to_dict(credentials):
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes}
+
 
